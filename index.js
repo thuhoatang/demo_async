3e8cec452be97a33ab2c2a58aff6b78eece91bd9
Minh Le <lnminh58@gmail.com>
2022-07-20 16:42:15 +0700

    add callback and homework

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 import { getUser, getUserBankAccount, getUserProfile } from './service';
+
+console.log('start fetch data');
+
+const callback = (result) => {
+  console.log('user data', result);
+  getUserBankAccount(result.id, (result) => {
+    console.log('bank data', result);
+  });
+
+  getUserProfile(result.id, (result) => {
+    console.log('user profile data', result);
+  });
+};
+
+getUser(callback);
+
+console.log('stop fetch data');
+
+// task: convert function in service.js to promise
+
+// call services using promise and async await
+// case 1: call getUserBankAccount after call getUserProfile
+// case 2: call getUserBankAccount and getUserProfile parallel
+// create variable to store data from services
